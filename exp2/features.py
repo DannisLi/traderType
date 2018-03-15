5284db26d481a3f95a76ff0d72feba622ddf4888
DannisLi <3092964463@qq.com>
2018-03-16 07:11:59 +0800

    20180316-1

--- exp2/features.py
+++ exp2/features.py
@@ -21,12 +21,18 @@ for account,is_momentum,is_reverse in cursor.fetchall():
         is_institution = 1
         age = None
         open_time = None
+        is_Beijing_or_Shanghai = None
     else:
         is_institution = 0
         try:
             age = 2017 - int(certificate_id[6:10])
+            if int(certificate_id[:2]) in [11, 31]:
+                is_Beijing_or_Shanghai = 1
+            else:
+                is_Beijing_or_Shanghai = 0
         except:
             age = None
+            is_Beijing_or_Shanghai = None
         open_time = (datetime.date(2017,1,1) - open_date).days
     # begin, end
     begin = max(datetime.date(2014,1,1), open_date)
@@ -37,9 +43,14 @@ for account,is_momentum,is_reverse in cursor.fetchall():
     # 处理forced_times
     cursor.execute("select count(*) from chengjiao where account=%s and force_offset='1'", (account,))
     forced_times = 1. * cursor.fetchone()[0] / (end-begin).days * 252
-    data.append([account, is_momentum, is_reverse, is_institution, age, open_time, trade_times, forced_times])
+    # 处理leverage
+    cursor.execute("select avg(leverage) from leverage where account=%s", (account,))
+    leverage = cursor.fetchone()[0]
+    data.append([account, is_momentum, is_reverse, is_institution, age, open_time, trade_times, 
+                 forced_times, is_Beijing_or_Shanghai, leverage])
     
-df = pd.DataFrame(data, columns=['account', 'is_momentum', 'is_reverse', 'is_institution', 'age', 'open_time', 'trade_times', 'forced_times'])
+df = pd.DataFrame(data, columns=['account', 'is_momentum', 'is_reverse', 'is_institution', 'age', 
+                                 'open_time', 'trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage'])
 df.to_csv('features.csv')
 
 cursor.close()
