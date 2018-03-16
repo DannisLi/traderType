6e209c91a2a399ca2973743a2a3de709a295ee7b
DannisLi <3092964463@qq.com>
2018-03-16 19:28:49 +0800

    20180316-5

--- exp2/reg.py
+++ exp2/reg.py
@@ -31,5 +31,35 @@ for field in fields:
     print ''
     
 # 机构投资者全部特征回归
+samples = df[['is_momentum', 'is_reverse', 'trade_times', 'forced_times', 'leverage']]
+samples.dropna(axis=0, how='any', inplace=True)
+X = sm.add_constant(samples[['trade_times', 'forced_times', 'leverage']].values)
+# is_monentum
+Y = samples.is_momentum.values
+model = sm.Logit(Y, X)
+result = model.fit()
+print result.summary(yname='is_momentum', xname=['intercept', 'trade_times', 'forced_times', 'leverage'])
+print ''
+# is_reverse
+Y = samples.is_reverse.values
+model = sm.Logit(Y, X)
+result = model.fit()
+print result.summary(yname='is_reverse', xname=['intercept', 'trade_times', 'forced_times', 'leverage'])
+print ''
 
 # 非机构投资者全部特征回归
+samples = df[['is_momentum', 'is_reverse', 'age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage']]
+samples.dropna(axis=0, how='any', inplace=True)
+X = sm.add_constant(samples[['age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage']].values)
+# is_monentum
+Y = samples.is_momentum.values
+model = sm.Logit(Y, X)
+result = model.fit()
+print result.summary(yname='is_momentum', xname=['intercept', 'age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage'])
+print ''
+# is_reverse
+Y = samples.is_reverse.values
+model = sm.Logit(Y, X)
+result = model.fit()
+print result.summary(yname='is_reverse', xname=['intercept', 'age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage'])
+print ''
