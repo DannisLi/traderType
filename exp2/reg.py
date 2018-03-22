06ef09216948933637dea2a382665bc0089dc825
DannisLi <3092964463@qq.com>
2018-03-23 00:18:28 +0800

    真麻烦~

--- exp2/reg.py
+++ exp2/reg.py
@@ -5,45 +5,38 @@ import statsmodels.api as sm
 
 df = pd.read_csv('features.csv', header='infer', index_col=0)
 
-fields = df.columns[3:]
+features = df.columns[3:]
 
 # 逐个特征回归
-for field in fields:
-    is_m = []
-    is_r = []
-    x = []
-    samples = df[['is_momentum','is_reverse',field]]
+for fea in features:
+    # 洗掉样本中的缺失值
+    samples = df[['is_momentum', 'is_reverse', fea]]
     samples.dropna(axis=0, how='any', inplace=True)
-    for index,row in samples.iterrows():
-        is_m.append(row['is_momentum'])
-        is_r.append(row['is_reverse'])
-        x.append(row[field])
-    x = sm.add_constant(x)
-    
-    model = sm.Logit(is_m, x)
+    # 自变量中添加截距项
+    x = sm.add_constant(samples[fea].values)
+    # 回归动量型
+    model = sm.Logit(samples.is_momentum, x)
     result = model.fit()
-    print result.summary(yname='is_momentum', xname=['intercept', field])
+    print result.summary(yname='is_momentum', xname=['intercept', fea])
     print ''
-    
-    model = sm.Logit(is_r, x)
+    # 回归逆向型
+    model = sm.Logit(samples.is_reverse, x)
     result = model.fit()
-    print result.summary(yname='is_reverse', xname=['intercept', field])
+    print result.summary(yname='is_reverse', xname=['intercept', fea])
     print ''
     
 # 机构投资者全部特征回归
 samples = df[df.is_institution==1]
 samples = samples[['is_momentum', 'is_reverse', 'trade_times', 'forced_times', 'leverage']]
 samples.dropna(axis=0, how='any', inplace=True)
-X = sm.add_constant(samples[['trade_times', 'forced_times', 'leverage']].values)
-# is_monentum
-Y = samples.is_momentum.values
-model = sm.Logit(Y, X)
+x = sm.add_constant(samples[['trade_times', 'forced_times', 'leverage']].values)
+# 回归动量型
+model = sm.Logit(samples.is_momentum, x)
 result = model.fit()
 print result.summary(yname='is_momentum', xname=['intercept', 'trade_times', 'forced_times', 'leverage'])
 print ''
-# is_reverse
-Y = samples.is_reverse.values
-model = sm.Logit(Y, X)
+# 回归逆向型
+model = sm.Logit(samples.is_reverse, x)
 result = model.fit()
 print result.summary(yname='is_reverse', xname=['intercept', 'trade_times', 'forced_times', 'leverage'])
 print ''
@@ -54,13 +47,13 @@ samples = samples[['is_momentum', 'is_reverse', 'age', 'open_time','trade_times'
 samples.dropna(axis=0, how='any', inplace=True)
 X = sm.add_constant(samples[['age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage']].values)
 # is_monentum
-Y = samples.is_momentum.values
+Y = samples.is_momentum
 model = sm.Logit(Y, X)
 result = model.fit()
 print result.summary(yname='is_momentum', xname=['intercept', 'age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage'])
 print ''
 # is_reverse
-Y = samples.is_reverse.values
+Y = samples.is_reverse
 model = sm.Logit(Y, X)
 result = model.fit()
 print result.summary(yname='is_reverse', xname=['intercept', 'age', 'open_time','trade_times', 'forced_times', 'is_Beijing_or_Shanghai', 'leverage'])
